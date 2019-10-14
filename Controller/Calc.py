f75a61a3c7f9abc6287c0543dbad354b4b9873a4
shubhas <subash@prixa.org>
2019-10-14 17:51:16 +0545

    test cases added

---
+++ Controller/Calc.py
@@ -0,0 +1,14 @@
+
+class Calc:
+    def __init__(self):
+        print(123)
+
+    @staticmethod
+    def add(a, b):
+        """
+
+        :param a:
+        :param b:
+        :return:
+        """
+        return a + b
